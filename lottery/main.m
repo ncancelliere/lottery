8507b490300b0085c8c69b7dfbdc61876b06f876
Nicholas Cancelliere <ncancelliere@gmail.com>
2011-03-27 18:06:31 -0500

    Initial Commit

---
+++ lottery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lottery
+//
+//  Created by Nicholas Cancelliere on 3/27/11.
+//  Copyright 2011 Ozmox.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
